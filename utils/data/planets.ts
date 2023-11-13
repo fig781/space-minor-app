d68e4ecb80fad87e46f0f7e274a4067e23df0f4a
Aden Eilers <adeneilers@gmail.com>
2023-11-12 22:00:06 -0800

    planet and mineral icons

--- utils/data/planets.ts
+++ utils/data/planets.ts
@@ -1,47 +1,109 @@
 import { Planet } from '../types/planet.interface';
 import { itemsPlanet0 } from './items';
-import { anomaliesOnPlanet0 } from './planetAnomalies';
+// import { anomaliesOnPlanet0 } from './planetAnomalies';
 
+// planet ids must be unique
+// Ingredior
 export const planetsInSolar0: Planet[] = [
   {
     id: 0,
-    name: 'testplanet1',
-    icon: require('../../assets/GameIcons/Planets/Baren.png'),
-    type: 'solid planet',
+    name: 'Ingredior Star',
+    icon: require('../../assets/GameIcons/Planets/Stars/1.png'),
+    type: 'star',
     minerals: itemsPlanet0,
-    anomalies: anomaliesOnPlanet0,
+    anomalies: [],
   },
   {
     id: 1,
-    name: 'testplanet2',
-    icon: require('../../assets/GameIcons/Planets/Baren.png'),
+    name: 'Ingredior I',
+    icon: require('../../assets/GameIcons/Planets/Baron/1.png'),
     type: 'solid planet',
     minerals: [],
     anomalies: [],
   },
   {
     id: 2,
-    name: 'testplanet3',
-    icon: require('../../assets/GameIcons/Planets/Ice.png'),
-    type: 'solid planet',
+    name: 'Asteroid Cluster',
+    icon: require('../../assets/GameIcons/Planets/Asteroid/7.png'),
+    type: 'asteroid',
     minerals: [],
     anomalies: [],
   },
   {
     id: 3,
-    name: 'testplanet4',
-    icon: require('../../assets/GameIcons/Planets/Lava.png'),
+    name: 'Ingredior II',
+    icon: require('../../assets/GameIcons/Planets/Forest/3.png'),
     type: 'solid planet',
+    minerals: [],
+    anomalies: [],
+  },
+  {
+    id: 4,
+    name: 'Ingredior III',
+    icon: require('../../assets/GameIcons/Planets/Gas/1.png'),
+    type: 'gas planet',
+    minerals: [],
+    anomalies: [],
+  },
+  {
+    id: 5,
+    name: 'Ingredior IV',
+    icon: require('../../assets/GameIcons/Planets/Rocky/1.png'),
+    type: 'solid planet',
+    minerals: [],
+    anomalies: [],
+  }
+];
+
+// Caelus
+export const planetsInSolar1: Planet[] = [
+  {
+    id: 11,
+    name: 'Caelus Star',
+    icon: require('../../assets/GameIcons/Planets/Stars/9.png'),
+    type: 'star',
+    minerals: [],
+    anomalies: [],
+  },
+  {
+    id: 12,
+    name: 'Caelus I',
+    icon: require('../../assets/GameIcons/Planets/Desert/1.png'),
+    type: 'solid planet',
+    minerals: [],
+    anomalies: [],
+  },
+  {
+    id: 13,
+    name: 'Caelus II',
+    icon: require('../../assets/GameIcons/Planets/Gas/14.png'),
+    type: 'gas planet',
+    minerals: [],
+    anomalies: [],
+  },
+  {
+    id: 14,
+    name: 'Caelus III',
+    icon: require('../../assets/GameIcons/Planets/Gas/9.png'),
+    type: 'gas planet',
+    minerals: [],
+    anomalies: [],
+  },
+  {
+    id: 15,
+    name: 'Asteroid Cluster',
+    icon: require('../../assets/GameIcons/Planets/Asteroid/1.png'),
+    type: 'asteroid',
     minerals: [],
     anomalies: [],
   },
 ];
 
-export const planetsInSolar1: Planet[] = [
+export const planetsInSolar2: Planet[] = [
   {
-    id: 1,
+    id: 22,
     name: 'test',
-    icon: require('../../assets/GameIcons/Planets/Terran.png'),
+    icon: require('../../assets/GameIcons/Planets/Ocean/1.png'),
     type: 'solid planet',
     minerals: [],
     anomalies: [],
--- utils/data/solarSystems.ts
+++ utils/data/solarSystems.ts
@@ -1,18 +1,24 @@
 import { SolarSystem } from '../types/solarSystem.interface';
-import { planetsInSolar0, planetsInSolar1 } from './planets';
+import { planetsInSolar0, planetsInSolar1, planetsInSolar2 } from './planets';
 
 const solarSystems: SolarSystem[] = [
   {
     id: 0,
-    name: 'test',
+    name: 'Ingredior',
     icon: 'test',
     planets: planetsInSolar0,
   },
   {
     id: 1,
-    name: 'test2',
+    name: 'Caelus',
     icon: 'test',
     planets: planetsInSolar1,
+  },
+  {
+    id: 2,
+    name: 'Iter',
+    icon: 'test',
+    planets: planetsInSolar2,
   },
 ];
 
